5b3d9c3bad80864980a4e96ee1c18f4aa4037b8e
Farit Sibgatulin <farit@zelax.ru>
2024-04-23 15:38:32 +0300

    delete comment in test file

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
-"""
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -14,7 +14,7 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-"""
+
 
 def test_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"

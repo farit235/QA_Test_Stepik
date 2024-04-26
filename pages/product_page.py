0d15f2c9e9b0e1e98624807ed7d218f5453f8285
Farit Sibgatulin <farit@zelax.ru>
2024-04-26 16:21:50 +0300

    Add negative checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,8 @@
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, TimeoutException
 import math
 
 
@@ -43,4 +44,20 @@ class ProductPage(BasePage):
         else:
             return False
 
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((how, what))
+            )
+        except TimeoutException:
+            return True
+        return False
 
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(
+                EC.presence_of_element_located((how, what))
+            )
+        except TimeoutException:
+            return False
+        return True
--- test_product_page.py
+++ test_product_page.py
@@ -1,17 +1,26 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.locators import ProductPageLocators
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.parametrize(
+    "link",
+    [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param(
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+            marks=pytest.mark.xfail,
+        ),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+    ],
+)
 def test_guest_can_add_product_to_basket(browser, link):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -19,3 +28,36 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     result = page.click_on_add_cart_button()
     assert True == result, "Incorrect name or price of item"
+
+
+# Ниже описаны отрицательные проверки.
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_on_add_cart_button()
+    # You can write assert like this or how it was in  test_guest_cant_see_success_message function
+    timeout = page.is_not_element_present(*ProductPageLocators.PRODUCT_TITLE)
+    assert timeout == True, "Has message about adding a cart, expected False"
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    # This variant of use is better than in upper function which is called
+    # test_guest_cant_see_success_message_after_adding_product_to_basket
+    assert page.is_not_element_present(
+        *ProductPageLocators.ADDED_PRODUCT_TITLE
+    ), "Success message is presented, but should not be"
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_on_add_cart_button()
+    assert page.is_disappeared(
+        *ProductPageLocators.ADDED_PRODUCT_TITLE
+    ), "Wait until message will be deleted, but won't be"
